ed2aec8078145750158c9729135672f6287eb0a2
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-05 20:15:45 +0200

    Improve MongoDB connection error handling

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Exit process with failure
+  });
 
 // Define routes
 app.get('/', (req, res) => {
