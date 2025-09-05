3971198b1a24750bcd976924f7d857a2fdc574a5
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-05 20:13:58 +0200

    Update MongoDB connection options to suppress deprecation warnings.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
